b42a4959899afcb8665b29b85573daffe48170ad
Oleksii Kutsenko <kutsenko.new@gmail.com>
2022-12-19 22:47:43 +0100

    Summator has been added

--- app/countries/views.py
+++ app/countries/views.py
@@ -1,5 +1,10 @@
+from typing import Any
+
+import pandas as pd
 from django.db.models import QuerySet
 from rest_framework import viewsets
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 from countries.models import (
     Country,
@@ -10,6 +15,9 @@ from countries.serializers import CountrySerializer
 
 
 class CountryViewSet(viewsets.ModelViewSet):
+    """
+    List of countries with their average economic freedom and paying taxes normalized index scores.
+    """
     serializer_class = CountrySerializer
 
     def get_queryset(self) -> QuerySet:
@@ -28,7 +36,7 @@ class CountryViewSet(viewsets.ModelViewSet):
                 countryeconomicfreedomindex__year=latest_economic_freedom_index_year,
                 countrypayingtaxesindex__year=latest_paying_taxes_index_year,
             )
-            .values(
+            .values_list(
                 "name",
                 "countryeconomicfreedomindex__score",
                 "countryeconomicfreedomindex__year",
@@ -38,3 +46,36 @@ class CountryViewSet(viewsets.ModelViewSet):
         )
 
         return queryset
+
+    def list(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        data = list(self.get_queryset())
+        countries_dataframe = pd.DataFrame(
+            data,
+            columns=[
+                "name",
+                "economic_freedom",
+                "economic_freedom_year",
+                "paying_taxes",
+                "paying_taxes_year",
+            ],
+        )
+        max_economic_freedom_value = countries_dataframe["economic_freedom"].max()
+        min_economic_freedom_value = countries_dataframe["economic_freedom"].min()
+        countries_dataframe["economic_freedom_score"] = (
+            countries_dataframe["economic_freedom"] - min_economic_freedom_value
+        ) / (max_economic_freedom_value - min_economic_freedom_value)
+
+        max_paying_taxes_value = countries_dataframe["paying_taxes"].max()
+        min_paying_taxes_value = countries_dataframe["paying_taxes"].min()
+        countries_dataframe["paying_taxes_score"] = (
+            countries_dataframe["paying_taxes"] - min_paying_taxes_value
+        ) / (max_paying_taxes_value - min_paying_taxes_value)
+
+        countries_dataframe["total_score"] = countries_dataframe[
+            ["economic_freedom_score", "paying_taxes_score"]
+        ].mean(axis=1)
+        countries_dataframe = countries_dataframe.sort_values(
+            by="total_score", ascending=False
+        )
+
+        return Response(countries_dataframe.to_dict("records"))
